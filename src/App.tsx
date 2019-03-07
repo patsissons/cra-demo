7692cdc78c0feb3e02b3ba2bb4130d31f79acf16
Pat Sissons <pat.sissons@shopify.com>
2019-03-06 17:27:56 -0800

    addressing eslint violations

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import * as React from 'react';
 
 import {
   DisplayText,
@@ -12,22 +12,20 @@ import {
 import logo from './logo.svg';
 import styles from './App.module.scss';
 
-export class App extends Component {
-  render() {
-    return (
-      <Layout>
-        <div className={styles.App}>
-          <Heading element="h1">
-            <DisplayText size="extraLarge">create-react-app Demo</DisplayText>
-          </Heading>
-          <Image alt="logo" className={styles.Logo} source={logo} />
-          <FooterHelp>
-            Built using <Link url="https://polaris.shopify.com">Polaris</Link>.
-          </FooterHelp>
-        </div>
-      </Layout>
-    );
-  }
+export function App() {
+  return (
+    <Layout>
+      <div className={styles.App}>
+        <Heading element="h1">
+          <DisplayText size="extraLarge">create-react-app Demo</DisplayText>
+        </Heading>
+        <Image alt="logo" className={styles.Logo} source={logo} />
+        <FooterHelp>
+          Built using <Link url="https://polaris.shopify.com">Polaris</Link>.
+        </FooterHelp>
+      </div>
+    </Layout>
+  );
 }
 
 export default App;
