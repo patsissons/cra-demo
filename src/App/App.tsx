bc9e5de2e08c28cb3fc5e4d01919aefdddc23082
Pat Sissons <pat.sissons@shopify.com>
2019-03-10 23:49:53 -0700

    updating app component to load data and render loading content

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,48 +1,53 @@
 import * as React from 'react';
 
 import {
-  DisplayText,
+  Card,
   FooterHelp,
-  Heading,
-  Image,
-  Layout,
+  Loading,
   Link,
+  Page,
+  SkeletonPage,
+  SkeletonBodyText,
 } from '@shopify/polaris';
 import {withI18n, WithI18nProps} from '@shopify/react-i18n';
 import {compose} from 'recompose';
 
-import {logo} from './images';
-import styles from './App.module.scss';
+import {withTodoListData, WithTodoListDataProps} from './withTodoListData';
 import en from './translations/en.json';
 
-type ComposedProps = WithI18nProps;
+export interface Props {}
 
-export function App({i18n}: ComposedProps) {
+type ComposedProps = Props & WithI18nProps & WithTodoListDataProps;
+
+export function App({i18n, items}: ComposedProps) {
+  if (!items) {
+    return (
+      <SkeletonPage primaryAction title={i18n.translate('App.title')}>
+        <Card sectioned>
+          <SkeletonBodyText />
+        </Card>
+        <Loading />
+      </SkeletonPage>
+    );
+  }
+
   return (
-    <Layout>
-      <div className={styles.App}>
-        <Heading element="h1">
-          <DisplayText size="extraLarge">
-            {i18n.translate('App.header')}
-          </DisplayText>
-        </Heading>
-        <Image alt="logo" className={styles.Logo} source={logo} />
-        <FooterHelp>
-          {i18n.translate('App.footer', {
-            polarisLink: (
-              <Link url="https://polaris.shopify.com">
-                {i18n.translate('App.polarisLink')}
-              </Link>
-            ),
-          })}
-          Built using <Link url="https://polaris.shopify.com">Polaris</Link>.
-        </FooterHelp>
-      </div>
-    </Layout>
+    <Page title={i18n.translate('App.title')}>
+      <FooterHelp>
+        {i18n.translate('App.footer', {
+          polarisLink: (
+            <Link url="https://polaris.shopify.com">
+              {i18n.translate('App.polarisLink')}
+            </Link>
+          ),
+        })}
+      </FooterHelp>
+    </Page>
   );
 }
 
 export default compose<ComposedProps, {}>(
+  withTodoListData(),
   withI18n({
     id: 'App',
     fallback: en,
