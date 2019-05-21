334eee22778beb968ec3868834495dd4eabdd1c2
Pat Sissons <pat.sissons@shopify.com>
2019-05-20 20:40:15 -0700

    publish to docs

---
+++ docs/precache-manifest.ff961f8260c7fbf45ff48abe005f987c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f96f1e2f3a299c1369d426be90d9cb7",
+    "url": "/cra-demo/index.html"
+  },
+  {
+    "revision": "49675544246d1111e658",
+    "url": "/cra-demo/static/css/2.525f0ac4.chunk.css"
+  },
+  {
+    "revision": "e27eb6c8622fad8e5d41",
+    "url": "/cra-demo/static/css/main.6b8a1148.chunk.css"
+  },
+  {
+    "revision": "49675544246d1111e658",
+    "url": "/cra-demo/static/js/2.fc5429bd.chunk.js"
+  },
+  {
+    "revision": "e27eb6c8622fad8e5d41",
+    "url": "/cra-demo/static/js/main.f95a9e02.chunk.js"
+  },
+  {
+    "revision": "09e953ce015c4bc475b2",
+    "url": "/cra-demo/static/js/runtime~main.52efd976.js"
+  }
+]);
